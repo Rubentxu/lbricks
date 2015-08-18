8b5a7e11500ea5922787a803be1387dd88bc6d2b
Rubentxu <rubentxu74@gmail.com>
2015-08-18 15:37:10 +0200

    creado game.go

--- entity.go
+++ entity.go
@@ -12,33 +12,70 @@ type GraphPool struct {
 	providers map[string]GraphProvider
 }
 
-func CreateGraphPool() *GraphPool {
-	providers := make(map[string]GraphProvider)
-	return &GraphPool{providers}
-}
-
-func (e *GraphPool) CreateLogicGraph(name string) (*flow.Graph, map[string]chan *Event) {
+func (gp *GraphPool) CreateLogicGraph(name string) (*flow.Graph, map[string]chan *Event) {
 	graph := new(flow.Graph)
 	graph.InitGraphState()
-	provider, ok := e.providers[name]
+	provider, ok := gp.providers[name]
 	if !ok {
 		panic(fmt.Sprintf("%s graphLogic does not exist", name))
 	}
 	return provider(graph)
 }
 
-type EntityProvider func(graphPool GraphPool) *Entity
+func (gp *GraphPool) AddProvider(name string, provider GraphProvider) {
+	if _, ok := gp.providers[name]; !ok {
+		gp.providers[name] = provider
+	}
+}
+
+func CreateGraphPool() *GraphPool {
+	providers := make(map[string]GraphProvider)
+	return &GraphPool{providers}
+}
+
+type Entity struct {
+	id          int
+	Tags        string
+	logicGraphs map[string]*flow.Graph
+}
+
+func (e *Entity) Id() int {
+	return e.id
+}
+
+type EntityProvider func(graphPool GraphPool) (*Entity, []*Sensor)
 
 type EntityPool struct {
+	idCount   uint64
 	entities  map[int]*Entity
 	providers map[string]EntityProvider
 	graphPool *GraphPool
 }
 
-type Entity struct {
-	id          int
-	tags        string
-	logicGraphs map[string]*flow.Graph
+func (ep *EntityPool) CreateEntity(name string) *Entity {
+	provider, ok := ep.providers[name]
+	if !ok {
+		panic(fmt.Sprintf("%s entity does not exist", name))
+	}
+	entity, sensors := provider(ep.graphPool)
+	entity.id = ep.idCount++ 
+	ep.AddEntity(entity)
+}
+
+func (ep *EntityPool) AddEntity(entity *Entity) {
+	if _, ok := ep.entities[entity.Id()]; !ok {
+		ep.entities[entity.Id()] = entity
+	}
+}
+
+func (ep *EntityPool) AddProvider(name string, provider EntityProvider) {
+	if _, ok := ep.providers[name]; !ok {
+		ep.providers[name] = provider
+	}
+}
+
+func (ep *EntityPool) RegisterSensors(sensors []*Sensor) {
+	
 }
 
 func CreateEntityPool() *EntityPool {
@@ -48,15 +85,7 @@ func CreateEntityPool() *EntityPool {
 	return &EntityPool{entities, providers, graphPool}
 }
 
-func (e *EntityPool) CreateEntity(name string) *Entity {
-	provider, ok := e.providers[name]
-	if !ok {
-		panic(fmt.Sprintf("%s entity does not exist", name))
-	}
-	return provider(graph)
-}
-
-func (e *EntityPool) initGraphEntity() *flow.Graph {
+/*func (e *EntityPool) initGraphEntity() *flow.Graph {
 	n := new(flow.Graph) // creates the object in heap
 	n.InitGraphState()   // allocates memory for the graph
 	// Add processes to the network
@@ -67,4 +96,4 @@ func (e *EntityPool) initGraphEntity() *flow.Graph {
 	// Our net has 1 inport mapped to greeter.Name
 	n.MapInPort("In", "greeter", "Name")
 	return n
-}
+}*/

e91a6c7964465c5683d36d8166c2e8cda837dfa8
Rubentxu <rubentxu74@gmail.com>
2015-08-28 15:56:16 +0200

    GameGraphs

--- game.go
+++ game.go
@@ -8,5 +8,20 @@ type Game struct {
 func (g *Game) InitContext() {
 	g.EventSystem := &EventSystem{}
 	graphPool := CreateGraphPool()
+	graphPool.AddProvider("preloadGraph", preloadGraph)
+	graphPool.AddProvider("setupGraph", setupGraph)
 	g.EntityPool = CreateEntityPool(graphPool)
 }
+
+
+func preloadGraph() (*flow.Graph, map[string]chan *EventPacked){
+	
+}
+
+
+
+func setupGraph() (*flow.Graph, map[string]chan *EventPacked){
+	
+}
+
+

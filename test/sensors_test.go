efa608e7e8b35fd5295ad4f781e58265123237fa
Rubentxu <rubentxu74@gmail.com>
2015-09-02 15:50:36 +0200

    Benchmark

--- test/sensors_test.go
+++ test/sensors_test.go
@@ -4,25 +4,50 @@ import (
 	"testing"
 	"github.com/Rubentxu/lbricks"
 	"github.com/trustmaster/goflow"
+	"runtime"
 )
+
 
 func TestMouseSensor(t *testing.T) {
 	d := lbricks.NewMouseSensor("test",1,lbricks.LEFT_BUTTON_UP)
+	in := make(chan lbricks.MouseEvent)
+	out := make(chan interface{})
+	d.Mode= flow.ComponentModeSync
+	d.MouseEvent = in
+	d.Out = out
+	flow.RunProc(d)
+	for i := 0.0; i < 10000000.0; i++ {
+		var ms = lbricks.MouseEvent{float32(i * 2.0), float32(i * 2.0), lbricks.LEFT_BUTTON_UP}
+		in <- ms
+		i2 := (<-out).(lbricks.MouseEvent)
+		ix2 := i * 2.0
+		if i2.PosX != float32(ix2) {
+			t.Errorf("Error en el test %f != %f", i2, ix2)
+		}
+	}
+	// Shutdown the component
+	close(in)
+}
+
+
+
+func  BenchmarkMouseSensor(b *testing.B) {
+	var m runtime.MemStats
+	d := lbricks.NewMouseSensor("test",1,lbricks.LEFT_BUTTON_UP)
+
 	in := make(chan *lbricks.MouseEvent)
 	out := make(chan interface{})
 	d.MouseEvent = in
 	d.Out = out
 	flow.RunProc(d)
 	for i := 0.0; i < 10000000.0; i++ {
-		var ms = &lbricks.MouseEvent{float32(i * 2.0), float32(i * 2.0), lbricks.LEFT_BUTTON_UP}
+		var ms = &lbricks.MouseEvent{float32(2.0), float32(2.0), lbricks.LEFT_BUTTON_UP}
 		in <- ms
-		i2 := (<-out).(*lbricks.MouseEvent)
-		ix2 := i * 2.0
-		if i2.PosX != float32(ix2) {
-			t.Errorf("Error en el test %f != %f", i2, ix2)
-		}
-
+		<-out
 	}
-	// Shutdown the component
 	close(in)
+	runtime.ReadMemStats(&m)
+	b.Logf("HeapSys %d, HeapAlloc %d,HeapIdle %d,HeapReleased %d\n", m.HeapSys,  m.HeapAlloc,
+		m.HeapIdle, m.HeapReleased)
 }
+

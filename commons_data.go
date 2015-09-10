bbdbdcd17354a275c234a4bc1052f71f28fbf022
Rubentxu <rubentxu74@gmail.com>
2015-09-10 14:55:53 +0200

    commons interfaces

--- commons_data.go
+++ commons_data.go
@@ -1,46 +1,59 @@
 package lbricks
 
-import (
-	"github.com/ungerik/go3d/vec2"
-	"github.com/Rubentxu/lbricks/engi"
-)
+type Disposable interface{
+	Dispose()
+}
+
+type Drawable interface {
+	Texture() int
+	Width() float32
+	Height() float32
+	View() (float32, float32, float32, float32)
+}
+
+type Batch interface {
+	Begin()
+	Draw(r Drawable, x, y, originX, originY, scaleX, scaleY, rotation float32, color uint32, transparency float32)
+	End()
+	SetProjection(width, height float32)
+}
 
 type Renderer interface {
-	Render(engi.Batch)
+	Render(Batch)
 }
 
-type Transform struct {
-	Position *vec2.T
-	Scale  	*vec2.T
-	Rotation float32
+type Transform interface {
+	Position() []float32
+	Scale()  	[]float32
+	Rotation() float32
 }
 
-func NewTransform() *Transform{
-	t:= new(Transform)
-	t.Position = vec2.Zero
-	t.Scale = vec2.UnitXY
-	return t
+
+type View interface {
+	Renderer
+	Transform
+	Drawable
+	Id() uint
+	Name() string
+	Anchor() []float32
+	Color() uint32
+	Alpha()	float32
+	Layer() uint
+
 }
 
-type View struct {
-	*Transform
+type Sprite struct {
+	Transform Transform
 	id 		uint
 	Name 	string
-	Anchor  *vec2.T
-	Color   uint32
-	Alpha   float32
+	Anchor   []float32
+	Color    uint32
+	Alpha    float32
+	Region   Drawable
 	Layer   uint
-	Region  *engi.Region
 }
 
-func NewView(id uint,name string)  {
-	v:= new(View)
-	v.id = id
-	v.Name = name
-	v.Anchor = vec2.Zero
-	v.Transform = NewTransform()
-}
-
-func (v *View) Render(batch *engi.Batch) {
-	batch.Draw(v.Region, v.Position[0], v.Position[1], v.Anchor[0], v.Anchor[1], v.Scale[0], v.Scale[1], v.Rotation, v.Color, v.Alpha)
+func (v *Sprite) Render(batch Batch) {
+	batch.Draw(v.Region, v.Transform.Position[0], v.Transform.Position[1], v.Anchor[0], v.Anchor[1], v.Transform.Scale[0],
+		v.Transform.Scale[1], v.Transform.Rotation, v.Color, v.Alpha)
 }

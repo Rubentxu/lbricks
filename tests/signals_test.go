14ae2c0547f036c21ec71916b2da4eb0a243650b
Rubentxu <rubentxu74@gmail.com>
2015-11-07 10:35:59 +0100

    cambios a reactive functional

---
+++ tests/signals_test.go
@@ -0,0 +1,25 @@
+package tests_test
+
+import (
+	"testing"
+	"github.com/Rubentxu/lbricks"
+	"fmt"
+	"time"
+)
+
+
+func TestSignal(t *testing.T) {
+	signal := lbricks.FromValues(1, 2, 3, 4, 5, 6, 7,8,9,10,11,12)
+
+	signal.Filter(func(value interface{}) bool {
+		return value.(int) % 3 == 0 }).
+	Map(func(value interface{}) interface{} {
+		return value.(int) * 2
+	}).
+	Subscribe(func (value interface {}) {
+		fmt.Println("From Subscribe ",value.(int))
+	})
+
+	time.Sleep(time.Nanosecond * 100000)
+}
+
